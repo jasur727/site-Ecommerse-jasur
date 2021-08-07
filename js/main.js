8a75c7c2c36edaf921a5c10ffa7e39463c5d2512
Jasur <developer7102@gmail.com>
2021-08-07 04:09:34 -0700

    add js categories qismi togirlandi

--- js/main.js
+++ js/main.js
@@ -6,3 +6,41 @@ if( elSiteFormToggle){
     elSiteFormSection.classList.toggle("form-open");
   });
 }
+
+// filter
+
+var ElOpenFilterTogler = document.querySelector('.filter-button__togler');
+var ELOpenFilter = document.querySelector('.categories-main__wrapper-one');
+
+if( ELOpenFilter){
+  ElOpenFilterTogler.addEventListener('click', function() {
+    ELOpenFilter.classList.toggle('filter--open');
+  });
+}
+
+
+var elCategoriesMainWrapper = document.querySelector('.categories-main__wrapper');
+var elCategoriesMainGridBtn = document.querySelector('.js-grid-btn');
+
+var elCategoriesMainListBtn = document.querySelector('.js-list-btn');
+
+function addGridClass() {
+  elCategoriesMainGridBtn.addEventListener('click', function() {
+    elCategoriesMainWrapper.classList.remove('list--list');
+
+    elCategoriesMainWrapper.classList.add('list--grid');
+  });
+}
+
+function addListClass() {
+  elCategoriesMainListBtn.addEventListener('click', function() {
+    elCategoriesMainWrapper.classList.remove('list--grid');
+
+    elCategoriesMainWrapper.classList.add('list--list');
+  });
+}
+
+if(elCategoriesMainListBtn && elCategoriesMainGridBtn) {
+  addListClass();
+  addGridClass();
+}
